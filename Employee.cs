a01e664d5f479515ebaa708b7af10e45e5bd6967
popmonac <78390497+popmonac@users.noreply.github.com>
2021-03-16 18:47:33 +0100

    Add files via upload

---
+++ Employee.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeTest
+{
+    class Employee
+    {
+        private string firstName;
+        private string lastName;
+        private decimal monthlySalary;
+
+        //construct that initializes the above varible
+        public Employee(string first, string last, decimal salary)
+        {
+            firstName = first;
+            lastName = last;
+            monthlySalary = salary;
+
+        }
+
+        //get and set properties for the FirstName
+        public string FirstName
+        {
+            get
+            {
+                return firstName;
+            }
+            set
+            {
+                firstName = value;
+            }
+        }
+
+        //get and set properties for the LastName
+        public string LastName
+        {
+            get
+            {
+                return lastName;
+            }
+            set
+            {
+                lastName = value;
+            }
+        }
+
+        //get and set properties for the Salary
+        public decimal MonthlySalary
+        {
+            get
+            {
+                return monthlySalary;
+            }
+            set
+            {
+             
+                if (value >= 0)
+                {
+                    monthlySalary = value;
+                  
+                }
+
+                else
+                {
+                    monthlySalary = 0;
+                    
+                }
+
+
+            }
+        }
+
+        //method handling the yearly salary
+        public decimal YearlySalary()
+        {
+            monthlySalary = monthlySalary * 12;
+            return monthlySalary;
+        
+        }
+
+        //method handling the yearly salary after 10% raise
+        public decimal YearlyRaise()
+        {
+
+            monthlySalary =  monthlySalary + (monthlySalary * 0.1M);
+            return monthlySalary;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee1 = new Employee("Lawal", "Ayodeji", 5000.00M ); //Employee1 object
+
+            Employee employee2 = new Employee("Japheth", "Khalid", 3000.00M); //Employee2 object
+
+            //Displaying Employee1 informations
+            Console.WriteLine ("Employee 1 = " + employee1.FirstName + " " + employee1.LastName);
+            Console.WriteLine ("{0}", "Yearly salary is = " + employee1.YearlySalary() ) ;
+
+            Console.WriteLine();
+            //Displaying Employee2 informations
+            Console.WriteLine("Employee 2 = " + employee2.FirstName + " " + employee2.LastName);
+            Console.WriteLine("{0}", "Yearly salary is = " + employee2.YearlySalary());
+
+            //Displaying the yearly salary after 10% raise
+            Console.WriteLine();
+            Console.WriteLine("Yearly salary review after 10% raise");
+            Console.WriteLine();
+
+            Console.WriteLine("MR " + employee1.FirstName + " " + employee1.LastName );
+            Console.WriteLine("{0}\n", "Yearly salary is = " + employee1.YearlyRaise());
+
+            Console.WriteLine();
+
+            Console.WriteLine("MR " + employee2.FirstName + " " + employee2.LastName );
+            Console.WriteLine("{0}\n", "Yearly salary is = " + employee2.YearlyRaise());
+
+
+            Console.ReadLine();
+        }
+    }
+}
